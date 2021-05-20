ee34f68a10f16960d9b0be2a255eb16af5d1ef32
Zain <zainofficial03@gmail.com>
2021-05-20 15:52:48 +0500

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Working_With_DLL;
+
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Demo d = new Demo();
+            Console.WriteLine("Enter Two Number");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n**********************\n");
+            c = d.Sum(a, b);
+            Console.WriteLine("Sum Of {0} + {1} = {2}", a, b, c);
+            Console.WriteLine("\n**********************\n");
+            c = d.Sub(a, b);
+            Console.WriteLine("Sub Of {0} - {1} = {2}", a, b, c);
+            Console.WriteLine("\n**********************\n");
+            c = d.Multi(a, b);
+            Console.WriteLine("Multi Of {0} * {1} = {2}", a, b, c);
+            Console.WriteLine("\n**********************\n");
+            c = d.Division(a, b);
+            Console.WriteLine("Sum Of {0} / {1} = {2}", a, b, c);
+        }
+    }
+}
---
+++ Working_With_DLL/Demo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Working_With_DLL
+{
+    public class Demo
+    {
+        public int Sum(int a , int b)
+        {
+            return a + b;
+        }
+
+        public int Sub(int a,int b)
+        {
+            return a - b;
+        }
+
+        public int Multi(int a,int b)
+        {
+            return a * b;
+        }
+
+        public int Division(int a,int b)
+        {
+            return a / b;
+        }
+    }
+}
